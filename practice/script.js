87b033d966c0cb25fde314e9fff49744fa8c6ce5
chidijacob <chidijacob54@gmail.com>
2022-10-04 21:34:29 +0100

    Add existing project files to Git

--- practice/script.js
+++ practice/script.js
@@ -20,3 +20,10 @@ const creationDiv = document.getElementById('created');
 const newElement = document.createElement('h1');
 newElement.innerHTML = 'i was created by science'
 creationDiv.appendChild(newElement);
+
+const new_me = document.createElement('p');
+new_me.innerHTML = 'i am a new para'
+const my_div = document.getElementById('demo');
+my_div.appendChild(new_me);
+new_me.style.color = 'red';
+new_me.style.fontSize = 'larger';
